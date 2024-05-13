ec8d3b7f7f4c150a84dbc11a7555d875f7cbe16a
Victor Oshiomah <81963885+VictorOshiomah@users.noreply.github.com>
2024-05-13 15:59:05 -0500

    ExpenseForm_ver1
    Automating Expense Entry Form

--- src/app/expense-entry/expense-entry.component.ts
+++ src/app/expense-entry/expense-entry.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-expense-entry',
   templateUrl: './expense-entry.component.html',
-  styleUrl: './expense-entry.component.css'
+  styleUrl: './expense-entry.component.css',
 })
 export class ExpenseEntryComponent {
-
+  amount: number;
+  date: any;
+paymentMethod: any;
 }
